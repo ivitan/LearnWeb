c65dda017c341e3ca3d0acac6a8da0e8fc571934
Vitan <vitan.me@gmail.com>
2018-09-19 14:39:09 +0800

    Update lianjia.aspx.cs

--- vitan/lianjia.aspx.cs
+++ vitan/lianjia.aspx.cs
@@ -14,13 +14,13 @@ public partial class lianjia : System.Web.UI.Page
     protected void Button1_Click(object sender, EventArgs e)
     {
         int i_start,i_end,i_sum = 0;
-		int i_start = int.Parse(TextBox1.Text);
-        int i_end = int.Parse(TextBox2.Text);
+	i_start = int.Parse(TextBox1.Text);
+        i_end = int.Parse(TextBox2.Text);
         
 		while (i_start <= i_end) {
 			i_sum += i_start;
 			i_start++;
 		}
-		label1.Text = TextBox1.Text + "连加到" + TextBox2.Text + "的和为：" + i_sum.Tostring()
+		label1.Text = TextBox1.Text + "连加到" + TextBox2.Text + "的和为：" + i_sum.ToString();
     }
 }
