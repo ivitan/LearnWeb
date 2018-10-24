26c9137f201b2109ef45b19b7e8f7ad328806f89
Vitan <vitan.me@gmail.com>
2018-10-24 17:43:13 +0800

    add QueryString

---
+++ C#/QueryString/Sample_02.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Sample_02 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void chk_1_CheckedChanged(object sender, EventArgs e)
+    {
+        string _checked = chk_1.Checked.ToString();
+        string _url = "Sample_02_targ.aspx?chk=" + _checked;
+        Server.Transfer(_url);
+
+    }
+    protected void btn_name_Click(object sender, EventArgs e)
+    {
+        string _name = txt_name.Text;
+        string _url = "Sample_02_targ.aspx?name=" + _name;
+        Response.Redirect(_url);
+
+    }
+    protected void btn_muilt_Click(object sender, EventArgs e)
+    {
+        string _soft = ddl_soft.Text, _website = ddl_website.Text;
+        //string _url = "Sample_02_targ.aspx?soft=" + _soft + "&website=" + _website;
+        string _url = "Sample_02_targ.aspx?soft="+_soft+"&website="+_website;
+        Response.Redirect(_url);
+
+    }
+}
---
+++ C#/QueryString/Sample_02_targ.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Sample_02_targ : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //在PageLoad中接收传递来的值
+
+        //接收ID值
+        //当没有参数时QueryString["id"] == null
+          lab_id.Text = Request.QueryString["id"];
+                  
+        //接收NAME值
+          lab_name.Text = Request.QueryString["name"];
+
+        //接收CHK值
+          lab_chk.Text = Request.QueryString["chk"];
+
+        //接收soft,website值,采用简略的写法
+          lab_soft.Text = Request["soft"];
+          lab_website.Text = Request["website"];
+
+           
+       
+
+    }
+}
